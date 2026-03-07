96f7a3da7ce1edd930e1cc0b85f39ed88a9cf490
nayanmhadase56-Naruto <nayanmhadase56@gmail.com>
2026-03-07 06:01:07 +0000

    Day 2 Variable

---
+++ 2.JSBASICS/Day2.js
@@ -0,0 +1,19 @@
+
+const AccountID =1891928
+let AccountName = "Naruto"
+var AccountEmail= "narut@google.com"
+var AccountPassword= "123445"
+Accountstate= "Mumbai"
+console.table([AccountID,AccountEmail,AccountPassword, Accountstate,AccountName]);
+AccountName = 2
+console.table([AccountID,AccountEmail,AccountPassword, Accountstate,AccountName]);
+//AccountID= 1223344
+//console.table([AccountID,AccountEmail,AccountPassword, Accountstate,AccountName]);
+
+/*
+Var is not used because of open scope
+*/
+let Name= "Rahul"
+let ExperienceYear= 3
+let AutomationTester= true
+console.log("Name",Name)
