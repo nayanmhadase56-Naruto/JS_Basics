a112642fb88611f01de1fcb6d316fff67acfd307
nayanmhadase56-Naruto <nayanmhadase56@gmail.com>
2026-03-09 05:00:32 +0000

    Day 4 completed

---
+++ 2.JSBASICS/Day4.js
@@ -0,0 +1,20 @@
+//comparsion operators
+
+let a= 67
+let b= 76
+
+console.log("A+B =" +  a+b) 
+console.log("A**B =" +  a**b) 
+console.log("A%B =" +  a%b) 
+console.log("A/B =" +  a/b) 
+68
+68
+68
+67
+67
+console.log("++A =" +  ++a) 
+console.log("A++ =" +  a++) 
+console.log("--A =" +  --a) 
+console.log("A-- =" +  a--) 
+console.log("A =" +  a) 
+console.log("A-- =" +  a--) 
