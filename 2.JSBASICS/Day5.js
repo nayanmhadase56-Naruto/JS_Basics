aff84ebc70241a12a5823e6be7647da2a20b809c
nayanmhadase56-Naruto <nayanmhadase56@gmail.com>
2026-03-10 05:33:29 +0000

    Done with day 5

---
+++ 2.JSBASICS/Day5.js
@@ -0,0 +1,31 @@
+//AAssignment operator
+
+let a = 1
+a+= 5
+console.log(a)
+let b = 1
+b = 5
+console.log(b)
+let c = 1
+c -= 5
+console.log(c)
+let d
+d *= 5
+console.log(d)
+
+let e =2
+e/= 5
+console.log(e)
+let f= 2
+f **= 5
+console.log(f)
+
+// Comparison operator
+
+let comp1 = 10
+let comp2 = "10";
+
+console.log("comp1 ==comp2" + comp1==comp2)
+console.log("comp1 !=comp2" + comp1!=comp2)
+console.log("comp1 ===comp2" + comp1===comp2)// it compare data type as well 
+console.log("comp1 !== comp2" + comp1!==comp2)
