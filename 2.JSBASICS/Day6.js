385b1e118408716e192eefa78a85157d46b436c0
nayanmhadase56-Naruto <nayanmhadase56@gmail.com>
2026-03-11 05:42:39 +0000

    Done with logical operator

---
+++ 2.JSBASICS/Day6.js
@@ -0,0 +1,11 @@
+//Logical operators is operators which gives boolean value base on the condition defined
+
+let a= 89
+let b =09
+
+console.log (a>=b && a!=b) // Combination of 2 values
+console.log (a<=b || a!=b) // If one value is true then it should be true
+console.log (!true)
+console.log (!false)
+console.log(!a) // 0      → false  non-zero numbers → true
+
