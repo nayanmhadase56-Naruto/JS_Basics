bc60dc7c52c4b7dabe42b4d72d706f00cc3070b2
nayanmhadase56-Naruto <nayanmhadase56@gmail.com>
2026-03-17 05:37:28 +0000

    Done

---
+++ 2.JSBASICS/day12.js
@@ -0,0 +1,77 @@
+/*const loginDetails={
+
+    username: "Naruto",
+    password: "123456"
+}
+
+let a= "Naruto"
+let b ="123456"
+
+if(loginDetails.username === a && loginDetails.password ===b)
+{
+    console.log("Welcome to homeepage")
+
+}
+else{
+
+    console.log("Username or password is incorrect")
+}
+*/
+
+const bankdetails={
+
+    cardNumber:"78999010",
+    pin:"1232",
+    accountType:"saving"
+
+}
+
+bankdetails.Balance = 10000
+//home page
+//accounttype
+//pin
+ console.log("Welcome to ATM")
+let c ="78999010"
+let d ="saving"
+let e ="1231"
+let f =11000
+if(bankdetails.cardNumber=== c){
+
+ console.log("Select Account type")
+
+if(bankdetails.accountType==d){
+
+     console.log("enter your Pin")
+
+
+if(bankdetails.pin==e){
+
+     console.log("enter Amount")
+
+
+if(bankdetails.Balance==f){
+
+     console.log("wait for the Cash do not remove card")
+}
+else  {
+
+     console.log("insuffcient funds")
+
+}
+
+}else{
+
+     
+     console.log("invalid pin")
+}
+}
+else{
+console.log("Wrong account type")
+     
+}
+}else {
+
+     console.log("invalid card")
+     
+}
+
