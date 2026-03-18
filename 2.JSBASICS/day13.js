7e8c3ee42f9dc555dfabf16110658fcc613f7bb7
nayanmhadase56-Naruto <nayanmhadase56@gmail.com>
2026-03-18 19:59:21 +0000

    Done

---
+++ 2.JSBASICS/day13.js
@@ -0,0 +1,17 @@
+
+
+for(let i=0; i<1000; i++){
+
+    console.log(i)
+}
+
+/*
+for (statement 1 ;Statement 2 ; Statement 3){
+    code to be exected
+}
+
+statement 1: will execute only once at intial leve
+Statement : it is condition base on loop run which will run until it get false
+statement 3: It will executed everytime when loop runs
+*/
+
